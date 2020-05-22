cdcd196f249cb291607cbc7f664d86af0775d618
dmknght <dmknght@parrotsec.org>
2020-05-23 04:12:34 +0700

    Move details to page of main widget

--- src/newGUISource.nim
+++ src/newGUISource.nim
@@ -1,59 +1,5 @@
 import gintro / [gtk, glib, gobject]#, notify, vte]
-
-
-proc showCurrentDetails(b: Button) =
-  #[
-    Show a new dialog about all details
-    Services:
-      Tor: is actived
-      AnonSurf: is actived
-    DNS:
-      DNS under tor
-    Buttons:
-      A button that can change id
-      Check IP
-    Monitor:
-      Nyx in vte terminal
-  ]#
-  #[
-    Box 1: vertical: AnonSurf, Tor, DNS details
-    Box 2: Horizontal: Buttons: changeID, checkIP
-    Box 3: VTE: Nyx
-  ]#
-
-  #[
-    Add status
-  ]#
-  let
-    statusDialog = newDialog()
-    statusArea = statusDialog.getContentArea()
-    boxStatus = newBox(Orientation.vertical, 3)
-    boxServicesStatus = newBox(Orientation.vertical, 3)
-    labelAnonStatus = newLabel("AnonSurf: Not running")
-    labelTor = newLabel("Tor: Not running")
-    labelDNS = newLabel("DNS: DNS status")
-
-  boxServicesStatus.add(labelAnonStatus)
-  boxServicesStatus.add(labelTor)
-  boxServicesStatus.add(labelDNS)
-  boxStatus.add(boxServicesStatus)
-
-  #[
-    Add buttons
-  ]#
-  let
-    boxStatusButtons = newBox(Orientation.horizontal, 3)
-    btnChangeID = newButton("Change ID")
-    btnCheckIP = newButton("Check IP")
-
-  boxStatusButtons.add(btnChangeID)
-  boxStatusButtons.add(btnCheckIP)
-
-  statusArea.add(boxStatus)
-  statusArea.add(boxStatusButtons)
-  
-  statusDialog.setTitle("AnonSurf Status")
-  statusDialog.showAll()
+import utils / status
 
 
 proc onClickDashboard(b: Button, s: Stack) =
@@ -69,13 +15,16 @@ proc onClickDashboard(b: Button, s: Stack) =
       s.setVisibleChildName("boot")
     elif b.label == "More Tools":
       s.setVisibleChildName("tools")
+    elif b.label == "Details":
+      s.setVisibleChildName("details")
 
 
 proc refreshStatus(): bool =
   #[
     Update current status everytime
   ]#
-  discard
+  let
+    basicStatus = getSurfStatus()
   return SOURCE_CONTINUE
 
 
@@ -131,7 +80,7 @@ proc createArea(boxMainWindow: Box) =
   boxStatusDetails.add(labelStatus)
   # boxStatus.
 
-  btnDetails.connect("clicked", showCurrentDetails)
+  # btnDetails.connect("clicked", showCurrentDetails)
   boxStatusDetails.add(btnDetails)
 
   boxStatusIcon.add(statusImage)
@@ -233,6 +182,65 @@ proc createArea(boxMainWindow: Box) =
 
   mainStack.addNamed(boxExtra, "tools")
   # End of extra dashboard
+
+  #[
+    Show a new dialog about all details
+    Services:
+      Tor: is actived
+      AnonSurf: is actived
+    DNS:
+      DNS under tor
+    Buttons:
+      A button that can change id
+      Check IP
+    Monitor:
+      Nyx in vte terminal - click button
+  ]#
+  #[
+    Box 1: vertical: AnonSurf, Tor, DNS details
+    Box 2: Horizontal: Buttons: changeID, checkIP
+  ]#
+
+  #[
+    Add status
+  ]#
+  let
+    boxStatusServices = newBox(Orientation.vertical, 3)
+    labelAnonStatus = newLabel("AnonSurf: Not running")
+    labelTor = newLabel("Tor: Not running")
+    labelDNS = newLabel("DNS: DNS status")
+
+  boxStatusServices.add(labelAnonStatus)
+  boxStatusServices.add(labelTor)
+  boxStatusServices.add(labelDNS)
+
+  #[
+    Add buttons
+  ]#
+  let
+    boxStatusButtons = newBox(Orientation.horizontal, 3)
+    btnChangeID = newButton("Change ID")
+    btnCheckIP = newButton("Check IP")
+    btnDetailsBack = newButton("Back")
+    
+  btnDetails.connect("clicked", onClickDashboard, mainStack)
+  boxStatusButtons.add(btnDetailsBack)
+  boxStatusButtons.add(btnChangeID)
+  boxStatusButtons.add(btnCheckIP)
+
+  let
+    boxDetails = newBox(Orientation.vertical, 3)
+
+  boxDetails.add(boxStatusServices)
+  boxDetails.add(boxStatusButtons)
+
+  boxDetails.showAll()
+  
+  btnDetailsBack.connect("clicked", onClickDashboard, mainStack)
+
+  mainStack.addNamed(boxDetails, "details")
+
+  # End of full details dashboard
   
   # Add everything to window
   boxMainWindow.add(mainStack)
