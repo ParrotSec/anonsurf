ec764e90f4c3921f3c044cc2d45e905ef91db3c8
dmknght <dmknght@localhost.localdomain>
2020-07-21 00:38:20 +0700

    correct location of nyx

--- nimsrc/anonsurf/actions/actMainPage.nim
+++ nimsrc/anonsurf/actions/actMainPage.nim
@@ -1,4 +1,4 @@
-import gintro / [gtk, glib]
+import gintro / [gtk, glib, vte]
 import .. / modules / [myip, torPorts]
 import .. / displays / noti
 import strutils
@@ -115,3 +115,33 @@ proc onClickChangeID*(b: Button) =
         "File not found",
         "error"
       )
+
+
+proc onClickTorStatus*(b: Button) =
+  #[
+    Spawn a native GTK terminal and run nyx with it to show current tor status
+  ]#
+  let
+    statusDialog = newDialog()
+    statusArea = statusDialog.getContentArea()
+    nyxTerm = newTerminal()
+
+  statusDialog.setTitle("Tor bandwidth")
+
+  nyxTerm.spawnAsync(
+    {noLastlog}, # pty flags
+    nil, # working directory
+    ["/usr/bin/nyx", "--config", "/etc/anonsurf/nyxrc"], # args
+    [], # envv
+    {doNotReapChild}, # spawn flag
+    nil, # Child setup
+    nil, # child setup data
+    nil, # chlid setup data destroy
+    -1, # timeout
+    nil, # cancellabel
+    nil, # callback
+    nil, # pointer
+  )
+
+  statusArea.packStart(nyxTerm, false, true, 3)
+  statusDialog.showAll()
