a3eba36dd94e8a5743a238856bd1988f60f20513
dmknght <dmknght@localhost.localdomain>
2020-09-16 01:16:58 +0700

    Remove idea

--- nimsrc/anonsurf/AnonSurfCli.nim
+++ nimsrc/anonsurf/AnonSurfCli.nim
@@ -15,7 +15,6 @@ proc askUser(): bool =
 
 
 proc killApps() =
-  # TODO better msg
   const
     killCommand = "killall -q chrome dropbox skype icedove thunderbird firefox firefox-esr chromium xchat hexchat transmission steam firejail /usr/lib/firefox/firefox"
     cacheCommand = "bleachbit -c adobe_reader.cache chromium.cache chromium.current_session chromium.history elinks.history emesene.cache epiphany.cache firefox.url_history flash.cache flash.cookies google_chrome.cache google_chrome.history  links2.history opera.cache opera.search_history opera.url_history &> /dev/null"
