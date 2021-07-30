d349dee9dc3a1d39e7c5102e569eb068bd7c0584
Nong Hoang Tu <dmknght@parrotsec.org>
2021-07-30 12:43:56 +0700

    sync changes

--- nimsrc/anonsurf/cores/handle_killapps.nim
+++ nimsrc/anonsurf/cores/handle_killapps.nim
@@ -1,12 +1,12 @@
 import os
-import activities / app_kills
+# import activities / app_kills
 
 
-proc cmd_kill_apps*(callback_send_messages: proc) =
+proc cmd_kill_apps*(callback_send_messages, handle_ansurf_sendkill: proc) =
   const
     path_bleachbit = "/usr/bin/bleachbit"
   
-  let kill_result = ansurf_kill_sendkill()
+  let kill_result = handle_ansurf_sendkill()
   if kill_result != 0:
     callback_send_messages("Kill apps", "Some apps coudn't be killed", 1)
   
@@ -14,3 +14,11 @@ proc cmd_kill_apps*(callback_send_messages: proc) =
     callback_send_messages("Kill apps", "Bleachbit is not found. Can't remove caches", 1)
   else:
     discard
+
+
+proc cmd_init_cli_askkill(is_desktop: bool) =
+  discard
+
+
+proc cmd_init_gtk_askkill() =
+  discard
