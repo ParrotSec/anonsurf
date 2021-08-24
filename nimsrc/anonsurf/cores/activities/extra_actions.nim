dddce3e4f04642e684ec3e23dced6026991605bb
Nong Hoang Tu <dmknght@parrotsec.org>
2021-08-24 08:21:56 +0700

    code clean

--- nimsrc/anonsurf/cores/activities/extra_actions.nim
+++ nimsrc/anonsurf/cores/activities/extra_actions.nim
@@ -41,13 +41,3 @@ proc ansurf_extra_changeID*(conf: string): seq[string] =
     let recvData = sock.recv(256).split("\n")
     sock.close()
     return recvData
-
-
-# proc ansurf_extra_killApps*(): int =
-#   const
-#     killCommand = "killall -q chrome dropbox skype icedove thunderbird firefox firefox-esr chromium xchat hexchat transmission steam firejail /usr/lib/firefox/firefox"
-#     cacheCommand = "bleachbit -c adobe_reader.cache chromium.cache chromium.session chromium.history chromium.form_history elinks.history emesene.cache epiphany.cache firefox.cache firefox.crash_reports firefox.url_history firefox.forms flash.cache flash.cookies google_chrome.cache google_chrome.history google_chrome.form_history google_chrome.search_engines google_chrome.session google_earth.temporary_files links2.history opera.cache opera.form_history opera.history &> /dev/null"
-#   let
-#     killResult = execCmd(killCommand)
-#     cacheResult = execCmd(cacheCommand)
-#   return killResult + cacheResult
