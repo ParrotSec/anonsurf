4c1e21498a7fa277133c073dda7f724612fd4b31
Nong Hoang Tu <dmknght@parrotsec.org>
2021-08-26 11:20:51 +0700

    code clean

--- nimsrc/anonsurf/gtk/gui_activities/main_widget_activities.nim
+++ nimsrc/anonsurf/gtk/gui_activities/main_widget_activities.nim
@@ -40,17 +40,13 @@ proc ansurf_gtk_do_start_stop*(b: Button, cb_send_msg: proc) =
 proc ansurf_gtk_do_restart*(b: Button, cb_send_msg: proc) =
   createThread(ansurf_workers_common, do_anonsurf_restart, cb_send_msg)
   # ansurf_workers_common.joinThread()
-  # createThread(ansurf_workers_common_sudo, ansurf_acts_handle_restart, ("gksudo", cb_send_msg))
-  # ansurf_acts_handle_restart("gksudo", cb_send_msg)
 
 
 proc ansurf_gtk_do_myip*(b: Button, cb_send_msg: proc) =
-  # ansurf_acts_handle_checkIP(cb_send_msg)
   createThread(ansurf_workers_myip, do_anonsurf_checkip, (cb_send_msg))
 
 
 proc ansurf_gtk_do_changeid*(b: Button, cb_send_msg: proc) =
-  # ansurf_acts_handle_changeID(cb_send_msg)
   createThread(ansurf_workers_common, do_anonsurf_changeid, (cb_send_msg))
   ansurf_workers_common.joinThread()
 
