7a0649da8f98a913a2c728903fbcf3b819354d4d
Nong Hoang Tu <dmknght@parrotsec.org>
2021-08-26 10:47:43 +0700

    Optimize code

--- nimsrc/anonsurf/gtk/gui_activities/main_widget_activities.nim
+++ nimsrc/anonsurf/gtk/gui_activities/main_widget_activities.nim
@@ -7,12 +7,12 @@ import .. / widgets / tor_status_widget
 let cb_kill_apps = init_gtk_askkill() 
 
 
-proc do_anonsurf_start(cb_send_msg: proc) {.gcsafe.} =
-  ansurf_acts_handle_start("gksudo", cb_kill_apps, cb_send_msg)
+# proc do_anonsurf_start(cb_send_msg: proc) {.gcsafe.} =
+#   ansurf_acts_handle_start("gksudo", cb_kill_apps, cb_send_msg)
 
 
-proc do_anonsurf_stop(cb_send_msg: proc) {.gcsafe.} =
-  ansurf_acts_handle_stop("gksudo", cb_kill_apps, cb_send_msg)
+# proc do_anonsurf_stop(cb_send_msg: proc) {.gcsafe.} =
+#   ansurf_acts_handle_stop("gksudo", cb_kill_apps, cb_send_msg)
 
 
 proc do_anonsurf_restart(cb_send_msg: proc) {.gcsafe.} =
@@ -30,12 +30,10 @@ proc do_anonsurf_checkip(cb_send_msg: proc) {.gcsafe.} =
 proc ansurf_gtk_do_start_stop*(b: Button, cb_send_msg: proc) =
   if b.label == "Start":
     # createThread(ansurf_workers_common, do_anonsurf_start, cb_send_msg)
-    # createThread(ansurf_start_stop_workers, ansurf_acts_handle_start, ("gksudo", cb_kill_apps, cb_send_msg))
     ansurf_acts_handle_start("gksudo", cb_kill_apps, cb_send_msg)
   else:
     # createThread(ansurf_workers_common, do_anonsurf_stop, cb_send_msg)
     ansurf_acts_handle_stop("gksudo", cb_kill_apps, cb_send_msg)
-    # createThread(ansurf_start_stop_workers, ansurf_acts_handle_stop, "gksudo", cb_kill_apps, cb_send_msg)
   # joinThread(ansurf_workers_common)
 
 
