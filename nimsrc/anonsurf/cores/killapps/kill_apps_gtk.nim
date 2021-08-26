1d93aa8d095963c5dcd6beabae0667553b6860d6
Nong Hoang Tu <dmknght@parrotsec.org>
2021-08-26 09:55:01 +0700

    Fix kill app for cli

--- nimsrc/anonsurf/cores/killapps/kill_apps_gtk.nim
+++ nimsrc/anonsurf/cores/killapps/kill_apps_gtk.nim
@@ -3,18 +3,17 @@ import .. / ansurf_types
 import kill_apps_activities
 
 
-# proc onClickExit(w: Window) =
-  # mainQuit()
+proc onClickExit(w: Window) =
+  mainQuit()
 
 
 proc do_not_kill(b: Button) =
-  discard
-  # mainQuit()
+  mainQuit()
 
 
 proc do_kill(b: Button, callback_send_msg: callback_send_messenger) =
   ansurf_kill_apps(callback_send_msg)
-  # mainQuit()
+  mainQuit()
 
 
 # proc do_exit(b: Button) =
@@ -58,27 +57,28 @@ proc window_kill_app*(callback_send_msg: callback_send_messenger) =
   mainBoard.setBorderWidth(3)
 
   mainBoard.showAll()
-  # mainBoard.connect("destroy", onClickExit)
+  mainBoard.connect("destroy", onClickExit)
+  gtk.main()
+  # mainQuit()
+
+
+proc window_kill_apps_gtk*(callback_send_msg: callback_send_messenger) =
+  gtk.init()
+  let
+    mainBoard = newWindow()
+    boxMainWindow = box_kill_app(callback_send_msg)
+  
+  mainBoard.setResizable(false)
+  mainBoard.title = "Kill dangerous application"
+  mainBoard.setPosition(WindowPosition.center)
+  mainBoard.add(boxMainWindow)
+  mainBoard.setBorderWidth(3)
+
+  mainBoard.showAll()
+  mainBoard.connect("destroy", onClickExit)
   gtk.main()
   mainQuit()
 
-
-# proc window_kill_apps_gtk*(callback_send_msg: callback_send_messenger) =
-#   gtk.init()
-#   let
-#     mainBoard = newWindow()
-#     boxMainWindow = box_kill_app(callback_send_msg)
-  
-#   mainBoard.setResizable(false)
-#   mainBoard.title = "Kill dangerous application"
-#   mainBoard.setPosition(WindowPosition.center)
-#   mainBoard.add(boxMainWindow)
-#   mainBoard.setBorderWidth(3)
-
-#   mainBoard.showAll()
-#   mainBoard.connect("destroy", onClickExit)
-#   gtk.main()
-#   mainQuit()
 
 proc dialog_kill_app*(callback_send_msg: callback_send_messenger) =
   # FIXME broken
