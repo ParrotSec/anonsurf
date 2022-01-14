b045ef35cf713ae751d5ae7428ea9d2d6b058658
Nong Hoang Tu <dmknght@parrotsec.org>
2022-01-14 08:55:13 +0700

    use pkexec instead of gksudo

--- nimsrc/anonsurf/cores/handle_activities.nim
+++ nimsrc/anonsurf/cores/handle_activities.nim
@@ -7,7 +7,7 @@ import strutils
 
 proc cmd_init_sudo*(isDesktop: bool): string =
   if isDesktop:
-    return "gksudo"
+    return "pkexec env DISPLAY=\"$DISPLAY\" XAUTHORITY=\"$XAUTHORITY\" "
   else:
     return "sudo"
 
