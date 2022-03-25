fe02f466713668f27add39eef2a9ca9b2eefa007
Nong Hoang Tu <dmknght@parrotsec.org>
2022-03-25 22:08:51 +0700

    First simple setting dialog

--- nimsrc/anonsurf/gtk/widgets/options_widget.nim
+++ nimsrc/anonsurf/gtk/widgets/options_widget.nim
@@ -2,4 +2,21 @@ import gintro / gtk
 
 
 proc onClickOptions*(b: Button) =
-  discard
+  let
+    dialogSettings = newDialog()
+    dialogArea = dialogSettings.getContentArea()
+    optionBridge = newComboBoxText()
+
+  optionBridge.appendText("No Bridge")
+  optionBridge.appendText("Automation")
+  optionBridge.appendText("Manual Selection")
+  optionBridge.active = 0
+
+
+  dialogSettings.setTitle("AnonSurf Settings")
+  dialogSEttings.setIconName("preferences-desktop")
+  
+  dialogArea.add(optionBridge)
+  dialogSettings.showAll()
+  discard dialogSettings.run()
+  dialogSettings.destroy()
