e7570bbc69e8151d507f1434d4420d0994f3f87d
Nong Hoang Tu <dmknght@parrotsec.org>
2022-05-05 18:25:30 +0700

    Add submenu with start / stop / restart/ status of anonsurf

--- nimsrc/anonsurf/gtk/ansurf_systray.nim
+++ nimsrc/anonsurf/gtk/ansurf_systray.nim
@@ -7,21 +7,34 @@ let cb_send_msg = cli_init_callback_msg(true)
 
 
 proc ansurf_right_click_menu*(i: StatusIcon, b: int, activeTime: int) =
-  # TODO open anonsurf
   # TODO refresh menu
   let
-    menu = newMenu()
+    right_click_menu = newMenu()
+    ansurf_control_menu = newMenu()
     item_my_ip = newMenuItemWithlabel("Check IP")
     item_quit = newMenuItemWithlabel("Quit")
+    item_start_surf = newMenuItemWithLabel("Start")
+    item_stop_surf = newMenuItemWithLabel("Stop")
+    item_restart_surf = newMenuItemWithLabel("Restart")
+    item_status_surf = newMenuItemWithLabel("Status")
+    item_ansurf_control = newMenuItemWithLabel("AnonSurf")
 
+  # Sub menu
+  ansurf_control_menu.append(item_start_surf)
+  ansurf_control_menu.append(item_stop_surf)
+  ansurf_control_menu.append(item_restart_surf)
+  ansurf_control_menu.append(item_status_surf)
+  item_ansurf_control.setSubmenu(ansurf_control_menu)
+
+  # Main menu
+  right_click_menu.append(item_ansurf_control)
   item_my_ip.connect("activate", ansurf_gtk_do_myip, cb_send_msg)
-  menu.append(item_my_ip)
-
+  right_click_menu.append(item_my_ip)
   item_quit.connect("activate", ansurf_gtk_do_stop)
-  menu.append(item_quit)
+  right_click_menu.append(item_quit)
 
-  menu.showAll()
-  menu.popup(nil, nil, nil, nil, b, activeTime)
+  right_click_menu.showAll()
+  right_click_menu.popup(nil, nil, nil, nil, b, activeTime)
 
 
 proc ansurf_left_click*(i: StatusIcon, w: Window) =
