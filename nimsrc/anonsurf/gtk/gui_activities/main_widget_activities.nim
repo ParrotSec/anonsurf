8f1cda9c17d02797f11eb4001b457181a2ec2c13
Nong Hoang Tu <dmknght@parrotsec.org>
2022-05-12 15:31:21 +0700

    remove unused comments

--- nimsrc/anonsurf/gtk/gui_activities/main_widget_activities.nim
+++ nimsrc/anonsurf/gtk/gui_activities/main_widget_activities.nim
@@ -10,10 +10,6 @@ proc do_anonsurf_start(cb_send_msg: proc) {.gcsafe.} =
   ansurf_acts_handle_start("menuexecg", cb_send_msg)
 
 
-# proc do_anonsurf_stop(cb_send_msg: proc) {.gcsafe.} =
-#   ansurf_acts_handle_stop("menuexecg", cb_send_msg)
-
-
 proc do_anonsurf_restart(cb_send_msg: proc) {.gcsafe.} =
   ansurf_acts_handle_restart("menuexecg", cb_send_msg)
 
@@ -28,16 +24,12 @@ proc do_anonsurf_checkip(cb_send_msg: proc) {.gcsafe.} =
 
 proc ansurf_gtk_do_start_stop*(b: Button, cb_send_msg: proc) =
   if b.label == "Start":
-    # cb_kill_apps(cb_send_msg)
     dialog_kill_app(cb_send_msg)
-    # ansurf_acts_handle_start("menuexecg", cb_send_msg)
     createThread(ansurf_workers_common, do_anonsurf_start, cb_send_msg)
   else:
-    # createThread(ansurf_workers_common, do_anonsurf_stop, cb_send_msg)
     ansurf_acts_handle_stop("menuexecg", cb_send_msg)
     if not getServStatus("anonsurfd"):
       dialog_kill_app(cb_send_msg)
-  # joinThread(ansurf_workers_common)
 
 
 proc ansurf_gtk_do_restart*(b: Button, cb_send_msg: proc) =
@@ -47,8 +39,6 @@ proc ansurf_gtk_do_restart*(b: Button, cb_send_msg: proc) =
 
 proc ansurf_gtk_do_myip*(b: Button | MenuItem, cb_send_msg: proc) =
   createThread(ansurf_workers_myip, do_anonsurf_checkip, (cb_send_msg))
-  # ansurf_workers_myip.joinThread()
-  # do_anonsurf_checkip(cb_send_msg)
 
 
 proc ansurf_gtk_do_changeid*(b: Button, cb_send_msg: proc) =
