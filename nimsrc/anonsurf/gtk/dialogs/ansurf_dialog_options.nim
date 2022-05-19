f2a094a7136c397d76c9f4b8375a0f04e021a037
Nong Hoang Tu <dmknght@parrotsec.org>
2022-05-19 15:48:05 +0700

    Add more options to settings

--- nimsrc/anonsurf/gtk/dialogs/ansurf_dialog_options.nim
+++ nimsrc/anonsurf/gtk/dialogs/ansurf_dialog_options.nim
@@ -1,22 +1,36 @@
 import gintro / gtk
 
 
+proc initOptionBridge(cb: ComboBoxText, active: int) =
+  cb.appendText("No Bridge")
+  cb.appendText("Automation")
+  cb.appendText("Manual")
+  cb.active = active 
+
+
+proc initEntryBridge(en: Entry) =
+  en.setPlaceholderText("Bridge address")
+  en.setTooltipText("We should put format here?")
+
+
 proc onClickOptions*(b: Button) =
   let
     dialogSettings = newDialog()
     dialogArea = dialogSettings.getContentArea()
     optionBridge = newComboBoxText()
+    addrBridge = newEntry()
+    optionSandbox = newCheckButton("Sandbox mode")
 
-  optionBridge.appendText("No Bridge")
-  optionBridge.appendText("Automation")
-  optionBridge.appendText("Manual Selection")
-  optionBridge.active = 0
+  initOptionBridge(optionBridge, 0) # TODO load active number from settings instead
+  initEntryBridge(addrBridge)
 
+  dialogArea.add(optionBridge)
+  dialogArea.add(addrBridge)
+  dialogArea.add(optionSandbox)
 
   dialogSettings.setTitle("AnonSurf Settings")
   dialogSEttings.setIconName("preferences-desktop")
 
-  dialogArea.add(optionBridge)
   dialogSettings.showAll()
   discard dialogSettings.run()
   dialogSettings.destroy()
