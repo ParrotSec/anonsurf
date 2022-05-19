f5f8448829ca568f54a595ff4628bf9aaea0a452
Nong Hoang Tu <dmknght@parrotsec.org>
2022-05-20 00:57:04 +0700

    Add option to block inbound traffic. force this feature to optional

--- nimsrc/anonsurf/gtk/dialogs/ansurf_dialog_options.nim
+++ nimsrc/anonsurf/gtk/dialogs/ansurf_dialog_options.nim
@@ -17,6 +17,10 @@ proc initOptionSandbox(cb: CheckButton) =
   cb.setTooltipText("Only work without bridge")
 
 
+proc initOptionBlockInbound(cb: CheckButton) =
+  cb.setTooltipText("Block all inbound traffic when AnonSurf is on")
+
+
 proc onClickOptions*(b: Button) =
   let
     dialogSettings = newDialog()
@@ -24,10 +28,12 @@ proc onClickOptions*(b: Button) =
     optionBridge = newComboBoxText()
     addrBridge = newEntry()
     optionSandbox = newCheckButton("Sandbox mode")
+    optionBlockInbound = newCheckButton("Block Inbound traffic")
 
   initOptionBridge(optionBridge, 0) # TODO load active number from settings instead
   initEntryBridge(addrBridge)
   initOptionSandbox(optionSandbox)
+  initOptionBlockInbound(optionBlockInbound)
 
   dialogArea.add(optionBridge)
   dialogArea.add(addrBridge)
