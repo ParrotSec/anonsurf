79c83c7da8458fac87c2a1841be6f4974a6573de
Nong Hoang Tu <dmknght@parrotsec.org>
2022-05-21 05:11:51 +0700

    Add some more tmp functions

--- nimsrc/dnstool/dnstool.nim
+++ nimsrc/dnstool/dnstool.nim
@@ -11,6 +11,12 @@ proc dhclient_lease_exists(): bool =
   return fileExists(dhclient_dns_file)
 
 
+proc system_dns_file_is_symlink(): bool =
+  if getFileInfo(system_dns_file, followSymlink = false).kind == pcLinkToFile:
+    return true
+  return false
+
+
 proc resolvconf_create_symlink() =
   try:
     createSymlink(resolvconf_dns_file, system_dns_file)
@@ -29,6 +35,7 @@ proc dhclient_create_dhcp_dns() =
   # TODO handle if it's wrong value
   try:
     writeFile(system_dns_file, "nameserver " & dns_addr)
+    # TODO create a hook script to keep this address after reboot
   except:
     echo("Failed to write DNS address to " & system_dns_file)
 
@@ -40,3 +47,26 @@ proc dnst_create_dhcp_with_resolvconf() =
     dhclient_create_dhcp_dns()
   else:
     echo "Can't find neither resolvconf nor dhclient. Try custom DNS addr"
+
+
+proc dnst_create_backup() =
+  if system_dns_file_is_symlink():
+    #[
+      /etc/resolv.conf is a symlink of resolvconf which is at
+      /run/resolvconf/resolv.conf so we don't create backup here
+      When there's no backup, dnstool will try create DHCP's DNS
+      which should create a symlink if resolvconf is installed
+    ]#
+    return
+
+  try:
+    copyFile(system_dns_file, system_dns_backup)
+  except:
+    echo("Failed to create backup file")
+
+
+proc dnst_restore_backup() =
+  try:
+    moveFile(system_dns_backup, system_dns_file)
+  except:
+    echo("Failed to restore backup")
