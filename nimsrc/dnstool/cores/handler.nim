5f9251a3f4dd784ef51ed01df601ac0c5739b535
Nong Hoang Tu <dmknght@parrotsec.org>
2022-05-27 00:08:58 +0700

    basic handle create symlink with resolvconf

--- nimsrc/dnstool/cores/handler.nim
+++ nimsrc/dnstool/cores/handler.nim
@@ -20,25 +20,39 @@ proc dnst_restore_backup() =
     print_error("Failed to restore backup")
 
 
+proc handle_create_resolvconf_symlink() =
+  if not tryRemoveFile(system_dns_file):
+    print_error("Failed to remove " & system_dns_file & " to create a new one")
+  else:
+    resolvconf_create_symlink()
+
+
 proc dnst_show_status*() =
   discard
 
 
-proc handle_addr_dhcp_only*() =
+proc handle_addr_dhcp_only() =
   if resolvconf_exists():
-    resolvconf_create_symlink()
+    handle_create_resolvconf_symlink()
   elif dhclient_binary_exists():
     dhclient_create_dhcp_dns()
   else:
-    print_error("Can't find neither resolvconf nor dhclient. Try custom DNS addr")
+    print_error("Can't find neither resolvconf nor dhclient. Try custom DNS addr.")
 
 
-proc handle_addr_custom_only*() =
+proc handle_addr_custom_only() =
   discard
 
 
-proc handle_addr_mix_with_dhcp*() =
-  discard
+proc handle_addr_mix_with_dhcp(list_addr: seq[string]) =
+  if resolvconf_exists():
+    handle_create_resolvconf_symlink()
+    write_dns_to_resolvconf_tail(list_addr)
+  elif dhclient_binary_exists():
+    # TODO handle mixed addr of dhclient and custom
+    discard
+  else:
+    discard # force writing custom addr only
 
 
 proc handle_create_backup*() =
@@ -72,13 +86,19 @@ proc handle_argv_missing*() =
 
 proc handle_create_dns_addr*(has_dhcp: bool, list_addr: seq[string]) =
   let final_list_addr = validate_dns_addr(list_addr)
+  # TODO better to do callback: write addr, call show status, .. if all proc has the same structure
   if not has_dhcp:
     if len(final_list_addr) == 0:
       print_error("There's no valid DNS addresses.")
+      return
     else:
       handle_addr_custom_only()
   else:
     if len(final_list_addr) == 0:
       handle_addr_dhcp_only()
     else:
-      handle_addr_mix_with_dhcp()
+      handle_addr_mix_with_dhcp(list_addr)
+
+  # echo "\n[*] Applied DNS settings"
+  # TODO only print completed if all functions are good
+  dnst_show_status()
