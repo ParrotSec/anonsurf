34d4e8a9930e7eed9b61ec2d5eddc7b9996f354e
Nong Hoang Tu <dmknght@parrotsec.org>
2022-05-27 02:51:18 +0700

    Improve dhclient code by calling pre-write functions

--- nimsrc/dnstool/cores/dhclient.nim
+++ nimsrc/dnstool/cores/dhclient.nim
@@ -1,5 +1,5 @@
 import os
-import .. / cores / dnstool_const
+import .. / cores / [dnstool_const, utils]
 import .. / cli / print
 import strutils
 
@@ -15,7 +15,7 @@ proc dhclient_binary_exists*(): bool =
 proc dhclient_parse_dns*(): string =
   for line in lines(dhclient_dns_file):
     if "domain-name-servers" in line:
-      return line.split(" ")[^1].replace(";", "")
+      return $line.split(" ")[^1].replace(";", "")
 
 
 proc dhclient_create_dhcp_dns*() =
@@ -24,9 +24,4 @@ proc dhclient_create_dhcp_dns*() =
     return
 
   let dns_addr = dhclient_parse_dns()
-  # TODO handle if it's wrong value
-  try:
-    writeFile(system_dns_file, "nameserver " & dns_addr)
-    # TODO create a hook script to keep this address after reboot
-  except:
-    print_error("Failed to write DNS address to " & system_dns_file)
+  write_dns_to_system(@[dns_addr])
