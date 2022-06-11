c48f9bc144132c5e0ef811f5979073f0eeee9378
Nong Hoang Tu <dmknght@parrotsec.org>
2022-06-11 13:00:05 +0700

    Move backend button to new module

--- nimsrc/anonsurf/gtk/dialogs/ansurf_dialog_options.nim
+++ nimsrc/anonsurf/gtk/dialogs/ansurf_dialog_options.nim
@@ -1,33 +1,7 @@
 import gintro / [gtk, gobject]
+import .. / gui_activities / dialog_options
 import .. / gui_activities / core_activities
 import .. / .. / options / [option_handler, option_objects]
-
-
-type
-  ApplyConfigObj = object
-    bridgeOption: ComboBoxText
-    bridgeAddr: Entry
-    sandboxMode: CheckButton
-
-
-proc onClickApplyConfig(b: Button, c: ApplyConfigObj) =
-  let config = SurfConfig(
-    option_sandbox: c.sandboxMode.getActive(),
-    option_bridge_mode: cast[BridgeMode](c.bridgeOption.getActive()),
-    option_bridge_address: c.bridgeAddr.getText(),
-  )
-  ansurf_gtk_save_config(config)
-
-
-proc onClickBridgeMode(c: ComboBoxText, e: Entry) =
-  if c.getActive == 2:
-    e.setSensitive(true)
-  else:
-    e.setSensitive(false)
-
-
-proc onClickCancel(b: Button, d: Dialog) =
-  ansurf_gtk_close_dialog(d)
 
 
 proc initOptionBridge(cb: ComboBoxText, active: int) =
