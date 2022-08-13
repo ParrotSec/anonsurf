68122ff4005c320f08c1ddc3675f411d2c07da6b
Nong Hoang Tu <dmknght@parrotsec.org>
2022-08-14 04:04:05 +0700

    Add if-pre-up hook script

--- nimsrc/dnstool/cores/hook_scripts.nim
+++ nimsrc/dnstool/cores/hook_scripts.nim
@@ -23,6 +23,7 @@ proc hook_script_init*() =
   hook_script_create_new(hook_script_if_down_path, hook_script_if_down_data)
   hook_script_create_new(hook_script_if_up_path, hook_script_if_up_data)
   hook_script_create_new(hook_script_sys_shutdown_path, hook_script_sys_shutdown_data)
+  hook_script_create_new(hook_script_sys_start_path, hook_script_sys_start_data)
 
 
 proc hook_script_finit*() =
@@ -30,3 +31,4 @@ proc hook_script_finit*() =
   hook_script_remove_hook(hook_script_if_down_path)
   hook_script_remove_hook(hook_script_if_up_path)
   hook_script_remove_hook(hook_script_sys_shutdown_path)
+  hook_script_remove_hook(hook_script_sys_start_path)
